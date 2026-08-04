0038b4fc57bc143812026e8fa356bbbaa0daa358
preetamkcs <preetamkcs@gmail.com>
2026-08-05 00:42:53 +0530

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.java
@@ -0,0 +1,29 @@
+class Solution {
+        public boolean isValid(String s) {
+        Deque<Character> bracketStack = new ArrayDeque<>();
+        Map<Character,Character> bracketMap=new HashMap<>();
+        bracketMap.put('[',']');
+        bracketMap.put('{','}');
+        bracketMap.put('(',')');
+        char[] charArray = s.toCharArray();
+
+        for (Character c : charArray) {
+            if (isOpenBracket(c, bracketMap)) {
+                bracketStack.push(c);
+            } else {
+                if (bracketStack.isEmpty()) {
+                    return false;
+                } else {
+                    if (!bracketMap.get(bracketStack.pop()).equals(c)) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return bracketStack.isEmpty();
+    }
+    
+    private boolean isOpenBracket(Character c, Map<Character, Character> bracketMap) {
+        return bracketMap.containsKey(c);
+    }
+}
