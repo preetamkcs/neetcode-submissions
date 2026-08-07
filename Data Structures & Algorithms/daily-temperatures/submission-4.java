86814d26fd43829bacad5475ce63caa3bcebdde5
preetamkcs <preetamkcs@gmail.com>
2026-08-08 00:41:03 +0530

    Update: daily-temperatures - submission-4

--- Data Structures & Algorithms/daily-temperatures/submission-4.java
+++ Data Structures & Algorithms/daily-temperatures/submission-4.java
@@ -3,12 +3,9 @@ class Solution {
         int length = temperatures.length;
         int[] result=new int[length];
         for (int i = 0; i < length; i++) {
-            int count=0;
             for (int j = i+1; j < length; j++) {
-                if (temperatures[i]>=temperatures[j]){
-                    count++;
-                } else {
-                    result[i]=++count;
+                if (temperatures[i]<temperatures[j]){
+                    result[i]= j-i;
                     break;
                 }
             }
